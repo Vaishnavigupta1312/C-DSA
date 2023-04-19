b138c60ccf9ea827c0b6839ca2cfbab6c1547fd6
Vaishnavi_Gupta <vaishgupta1312@gmail.com>
2023-04-19 23:48:14 +0530

    Bitwise operatos and for loops

---
+++ bitwise-op.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int a=4;
+     int b=6;
+    
+    //bitwise and(&) or(|) not(~) and xor(^)
+    cout<<"a&b = "<<(a&b) <<endl;
+    cout<<"a|b = "<<(a|b) <<endl;
+    cout<<"a^b = "<<(a^b) <<endl;
+    cout<<"~a = "<<(~a) <<endl;
+    cout<<"~b = "<<(~b) <<endl;
+
+    //left shift(*2) and right shift (/2)
+    cout<<(17>>1)<<endl;
+    cout<<(17>>2)<<endl;
+    cout<<(19<<1)<<endl;
+    cout<<(21<<2)<<endl;
+    
+
+
+}
