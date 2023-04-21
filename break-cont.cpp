3cc9f020b3480f72ed7bffd7919950b84b59db13
Vaishnavi_Gupta <vaishgupta1312@gmail.com>
2023-04-21 23:31:52 +0530

    Break and continue statement

---
+++ break-cont.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+// break statement - to come out of a loop
+
+
+
+// continue statement - to skip an iteration of the current loop
+}
